35b5c2cd79dc218e7014ba1f46a4f430b8a61b6b
rjrajput2811 <Bigboss2811@>
2025-05-17 14:12:38 +0530

    Add project files.

---
+++ CSO-Responsive/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CSO_Responsive.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
