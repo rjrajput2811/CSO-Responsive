69ca2b9dcaf87fb038be26df99e2a0def27a8c1f
Smita0404 <Saibex292929>
2025-05-31 19:55:30 +0530

    Layout and User page

---
+++ CSO-Responsive/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CSO_Responsive.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult User()
+        {
+            return View();
+        }
+
+        public IActionResult UserDetail()
+        {
+            return View();
+        }
+    }
+}
