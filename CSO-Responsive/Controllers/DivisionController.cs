1b012c2e8f4e5869924fbc988445f1a42965a049
rjrajput2811 <Bigboss2811@>
2025-06-07 11:33:42 +0530

    New changes-Smita

---
+++ CSO-Responsive/Controllers/DivisionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CSO_Responsive.Controllers
+{
+    public class DivisionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
