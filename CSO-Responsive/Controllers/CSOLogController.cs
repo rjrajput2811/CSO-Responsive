7271ce4e96c00a055bca02e03a88fb3fc0c5a981
rjrajput2811 <Bigboss2811@>
2025-06-17 15:25:08 +0530

    CSO Log

---
+++ CSO-Responsive/Controllers/CSOLogController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CSO_Responsive.Controllers
+{
+    public class CSOLogController : Controller
+    {
+        public IActionResult CSOLog()
+        {
+            return View();
+        }
+    }
+}
