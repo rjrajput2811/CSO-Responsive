9540bfc6080c0b43f07910e155fc2203f0b8d7d2
Vivek Jariwala <vivek666jariwala@gmail.com>
2025-07-02 17:05:55 +0530

    Set Image On DashBoard.

---
+++ CSO-Responsive/Controllers/DashBoardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CSO_Responsive.Controllers
+{
+    public class DashBoardController : Controller
+    {
+        public IActionResult DashBoard()
+        {
+            return View();
+        }
+    }
+}
