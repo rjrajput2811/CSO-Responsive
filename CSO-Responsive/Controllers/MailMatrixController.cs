2f5f1181c4e14f29c0dc62ad19490dbcf1c13f72
rjrajput2811 <Bigboss2811@>
2025-07-29 20:45:13 +0530

    MailMatrix UI DONE

---
+++ CSO-Responsive/Controllers/MailMatrixController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CSO_Responsive.Controllers
+{
+    public class MailMatrixController : Controller
+    {
+        public IActionResult MailMatrix()
+        {
+            return View();
+        }
+    }
+}
